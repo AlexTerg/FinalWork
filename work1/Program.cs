4602a48c2f7978be52d247902c2f402e5a99abe4
AlexTerg <s.tergoev@gmail.com>
2022-10-17 00:53:44 +0300

    function to create a new array of the required size

---
+++ work1/Program.cs
@@ -0,0 +1,41 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+
+PrintArray(array);
+Console.WriteLine();
+PrintArray(FindStringMoreThanThree(array));
+
+string[] CreateEmptyArray(string[] arr)
+{
+    int cnt = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            cnt++;
+        }
+    }
+    string[] array = new string[cnt];
+    return array;
+}
+
+string[] FindStringMoreThanThree(string[] arr)
+{
+    array = CreateEmptyArray(arr);
+    for (int i = 0, j = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            array[j] = arr[i];
+            j++;
+        }
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    foreach (var item in array)
+    {
+        Console.WriteLine(item);
+    }
+}
